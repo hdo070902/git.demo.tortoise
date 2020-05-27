ad3158fff7affd8d07d9a1b9655dda3a67011578
Tracy <hdo131906@gmail.com>
2020-05-27 11:38:01 -0400

    Initail

---
+++ git demo tortoise/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+		public static void main(String[] args) {
+		   SaleData data = new SaleData();
+		   
+		   displayGreeting();
+		   data.display();
+
+		}
+	    private static void displayGreeting(){
+	    	System.out.println("Hello Demo tortoise");
+	    	System.out.println("This App Shows demo tortoise");
+	    }
+	}
---
+++ git demo tortoise/src/SaleData.java
@@ -0,0 +1,7 @@
+
+public class SaleData {
+   private int data[] = {0,4,42};
+   public void display(){
+	   System.out.println("TODO: implement data display");
+   }
+}
