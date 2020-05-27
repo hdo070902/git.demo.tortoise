d95a74e8422fc28244c2322763f9e7d14dfc1cb0
Tracy <hdo131906@gmail.com>
2020-05-27 11:39:47 -0400

    Changed Saledata file

--- git demo tortoise/src/SaleData.java
+++ git demo tortoise/src/SaleData.java
@@ -3,5 +3,6 @@ public class SaleData {
    private int data[] = {0,4,42};
    public void display(){
 	   System.out.println("TODO: implement data display");
+	   System.out.println("TODO: Display demo tortoise");
    }
 }
